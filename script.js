b7a0e85e58e8652f8a3519174cb8075b04210611
Sara <moniquesarah5@gmail.com>
2025-01-08 13:04:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ function verificar(){
     let segun = document.getElementById('inum2')
     let message = document.getElementById('message')
 
+    let num1 = Number(prim.value)
+    let num2 = Number(segun.value)
+
     message.innerHTML = "Verificação:"
 
-    if(prim.value > segun.value){
+    if(num1 > num2){
         message.innerHTML = 'Os números são inválidos. Digite novamente'
         message.style.color = 'red' 
         message.style.display = 'block'
         message.appendChild(message)
     }else{
-        message.innerHTML = `Parabéns. Os números são válidos! O número ${segun.value} é maior que o número ${prim.value}`
+        message.innerHTML = `Parabéns. Os números são válidos! O número ${num2} é maior que o número ${num1}`
         message.style.color = 'green'
         message.style.display = 'block'
         message.appendChild(message)
